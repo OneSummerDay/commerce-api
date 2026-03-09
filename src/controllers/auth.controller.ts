e4654fa4c6fb92f64b7a84c9fe3f1ce2b3751946
OneSummerDay <artem.kraievyi@gmail.com>
2026-03-09 19:00:20 +0200

    feat: add password hashing for user registration

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,13 +1,16 @@
 import { Response, Request } from 'express';
+import bcrypt from 'bcrypt';
 import prisma from '../lib/prisma';
 
 export const register = async (req: Request, res: Response) => {
     const { email, password } = req.body;
 
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     const user = await prisma.user.create({
         data: {
             email,
-            password,
+            password: hashedPassword,
         }
     });
 
