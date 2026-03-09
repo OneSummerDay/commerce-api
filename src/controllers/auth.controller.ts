5fe5dfd425fe398f737943c068df237b8baad100
OneSummerDay <artem.kraievyi@gmail.com>
2026-03-09 18:54:11 +0200

    feat: add basic user registration with prisma

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,7 +1,18 @@
 import { Response, Request } from 'express';
+import prisma from '../lib/prisma';
 
 export const register = async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const user = await prisma.user.create({
+        data: {
+            email,
+            password,
+        }
+    });
+
     return res.status(201).json({
         message: 'User registered successfully',
+        user,
     });
 };
