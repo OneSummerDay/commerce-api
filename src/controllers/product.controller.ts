31ca8209b0272a88b348016d0b77f03c3262cffa
OneSummerDay <artem.kraievyi@gmail.com>
2026-03-11 20:56:00 +0200

    feat: add get product by id endpoint

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -72,3 +72,32 @@ export const createProduct = async (req: Request, res: Response) => {
     product,
   });
 };
+
+export const getProductById = async (req: Request, res: Response) => {
+  const productId = Number(req.params.id);
+
+  if (Number.isNaN(productId)) {
+    return res.status(400).json({
+      message: 'Product id must be a number',
+    });
+  }
+
+  const product = await prisma.product.findUnique({
+    where: {
+      id: productId,
+    },
+    include: {
+      category: true,
+    },
+  });
+
+  if (!product) {
+    return res.status(404).json({
+      message: 'Product not found',
+    });
+  }
+
+  return res.status(200).json({
+    product,
+  });
+};
--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { getProducts, createProduct } from '../controllers/product.controller';
+import { getProducts, createProduct, getProductById } from '../controllers/product.controller';
 
 
 const router = Router();
 
 router.get('/', getProducts);
+router.get('/:id', getProductById);
 router.post('/', createProduct);
 
 export default router;
